16c0d3cbc6417dca80db8001efa4d64209d5db98
keerthu1010 <keerthu1010@gmail.com>
2021-09-25 17:15:34 +0530

    Update 3

---
+++ 010-sum_of_primes_below_2million.js
@@ -0,0 +1,17 @@
+//Sum of all primes below 2000000
+function isPrime(a){
+    for (let i=2; i<a; i++){
+        if (a%i==0){
+            return false;
+        }
+    }
+    console.log(a);
+    return true;
+}
+let sum=0;
+for (let i=0; i<=2000000; i++){
+    if (isPrime(i)) {
+        sum += i;
+    }
+}
+console.log(sum);
